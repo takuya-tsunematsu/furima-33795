3dd84147e244a2eb3311cb447af5142ba5412b40
takuya-tsunematsu <takuya.07.tsunematsu@gmail.com>
2021-05-01 20:27:35 +0900

    ユーザー情報入力フォームの修正

--- db/migrate/20210424131919_devise_create_users.rb
+++ db/migrate/20210424131919_devise_create_users.rb
@@ -6,8 +6,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Database authenticatable
       t.string :nickname,              null: false, default: ""
       t.string :email,                 null: false, default: ""
-      t.string :password,              null: false, default: ""
-      t.string :password_confirmation, null: false, default: ""
+      t.string :encrypted_password,    null: false, default: ""
       t.string :last_name,             null: false, default: ""
       t.string :first_name,            null: false, default: ""
       t.string :last_name_kana,        null: false, default: ""
