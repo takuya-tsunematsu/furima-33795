ae458bc759227437c1e9f601c85e55f2388394c8
takuya-tsunematsu <takuya.07.tsunematsu@gmail.com>
2021-05-06 15:05:41 +0900

    validationエラーメッセージの表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,6 +12,17 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :shipping_date
 
-  validates :name,:price,:text,   presence: true
-  validates :category_id,:status_id,:delivery_charge_id,:prefecture_id,:shipping_date_id, numericality: { other_than: 1 } 
+  with_options presence: true do
+    validates :name
+    validates :text
+    validates :image
+    validates :price, format: {with: /\A[0-9]+\z/, message: 'Half-width number' },
+                      numericality: { only_integer: true,greater_than: 300, less_than: 9999999, message: 'Out of setting range'}
+
+    validates :category_id,         numericality: { other_than: 1, message:'Select'}
+    validates :status_id,           numericality: { other_than: 1 ,message:'Select'}
+    validates :delivery_charge_id,  numericality: { other_than: 1, message:'Select'}
+    validates :prefecture_id,       numericality: { other_than: 1, message:'Select'}
+    validates :shipping_date_id,    numericality: { other_than: 1, message:'Select'}
+  end
 end
