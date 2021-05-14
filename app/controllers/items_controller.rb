bcad3de160f65349a463deb7444e8e018669f39b
takuya-tsunematsu <takuya.07.tsunematsu@gmail.com>
2021-05-14 12:18:58 +0900

    【修正】before_actionの追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index,:show]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
     @items = Item.order("created_at DESC")
@@ -37,9 +38,20 @@ class ItemsController < ApplicationController
     end
   end
 
+  def destroy
+    @item = Item.find(params[:id])
+    redirect_to root_path unless current_user.id == @item.user_id
+    @item.destroy
+    redirect_to root_path
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:name, :price, :image, :text, :category_id ,:status_id, :delivery_charge_id, :prefecture_id, :shipping_date_id).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
